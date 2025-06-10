7daee89cb4d1c81d279d9ec4b95b3f05209e5377
Mariana Mena <maranashokry@ahoo.com>
2025-06-10 15:59:26 -0500

    Changes to be committed: 	modified:   .gitignore 	modified:   btre/settings.py 	new file:   btre/static/css/._all.css 	new file:   btre/static/css/._bootstrap.css 	new file:   btre/static/css/._lightbox.min.css 	new file:   btre/static/css/._style.css 	new file:   btre/static/img/._about.jpg 	new file:   btre/static/img/._building.jpg 	new file:   btre/static/img/._logo.png 	new file:   btre/static/img/._showcase.jpg 	new file:   btre/static/img/lightbox/._close.png 	new file:   btre/static/img/lightbox/._loading.gif 	new file:   btre/static/img/lightbox/._next.png 	new file:   btre/static/img/lightbox/._prev.png 	new file:   btre/static/js/._bootstrap.bundle.min.js 	new file:   btre/static/js/._jquery-3.3.1.min.js 	new file:   btre/static/js/._lightbox.min.js 	new file:   btre/static/js/._main.js 	new file:   btre/static/webfonts/._fa-brands-400.eot 	new file:   btre/static/webfonts/._fa-brands-400.svg 	new file:   btre/static/webfonts/._fa-brands-400.ttf 	new file:   btre/static/webfonts/._fa-brands-400.woff 	new file:   btre/static/webfonts/._fa-brands-400.woff2 	new file:   btre/static/webfonts/._fa-regular-400.eot 	new file:   btre/static/webfonts/._fa-regular-400.svg 	new file:   btre/static/webfonts/._fa-regular-400.ttf 	new file:   btre/static/webfonts/._fa-regular-400.woff 	new file:   btre/static/webfonts/._fa-regular-400.woff2 	new file:   btre/static/webfonts/._fa-solid-900.eot 	new file:   btre/static/webfonts/._fa-solid-900.svg 	new file:   btre/static/webfonts/._fa-solid-900.ttf 	new file:   btre/static/webfonts/._fa-solid-900.woff 	new file:   btre/static/webfonts/._fa-solid-900.woff2 	modified:   pages/views.py 	new file:   templates/base.html 	modified:   templates/pages/about.html 	modified:   templates/pages/index.html

--- pages/views.py
+++ pages/views.py
@@ -4,12 +4,10 @@ from django.http import HttpResponse
 def index(request):
     # """View function for the home page of the site."""
     return render(request, 'pages/index.html')  
-    # return HttpResponse('<h1>Welcome to the Home Page</h1><p>This is a simple Django application.</p>')
+    # return HttpResponse('<h1>Welcome to the Home Page</h1>')
 
 # Create your views here.
 def about(request):
-    # """View function for the about page of the site."""
-    # return render(request, 'pages/about.html', {})
     return render(request, 'pages/about.html', {
         'title': 'About Us',
         'content': 'This is the about page of our Django application. Here you can find information about our project and team.',
